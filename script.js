a84918d3aa9a1e07f117600c1aa7413a32b60f81
dog-names656 <amimsur6@gmail.com>
2025-03-08 19:51:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
-}
-showTime();
-setInterval(function () {
-	showTime();
-}, 1000);
+GameOverPanel.prototype.draw = function() {
+  var dimensions = GameOverPanel.dimensions;
+  var centerX = this.canvasDimensions.WIDTH / 2;
+  var textSourceX = dimensions.TEXT_X;
+  var textSourceY = dimensions.TEXT_Y;
+  var textSourceWidth = dimensions.TEXT_WIDTH;
+  var textSourceHeight = dimensions.TEXT_HEIGHT;
+  var textTargetX = Math.round(centerX - (dimensions.TEXT_WIDTH / 2));
+  var textTargetY = Math.round((this.canvasDimensions.HEIGHT - 25) / 3);
+  var textTargetWidth = dimensions.TEXT_WIDTH;
+  var textTargetHeight = dimensions.TEXT_HEIGHT;
+  var restartSourceWidth = dimensions.RESTART_WIDTH;
+  var restartSourceHeight = dimensions.RESTART_HEIGHT;
+  var restartTargetX = centerX - (dimensions.RESTART_WIDTH / 2);
+  var restartTargetY = this.canvasDimensions.HEIGHT / 2;
+  if (IS_HIDPI) {
+    textSourceY *= 2;
+    textSourceX *= 2;
+    textSourceWidth *= 2;
+    textSourceHeight *= 2;
+    restartSourceWidth *= 2;
+    restartSourceHeight *= 2;
+  }
+  // Custom GitHub message
+  this.canvasCtx.fillStyle = '#C9D1D9';
+  this.canvasCtx.font = '16px -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif';
+  this.canvasCtx.fillText('Merge Conflict Detected!', centerX - 70, textTargetY - 20);
+  // Original Game Over text
+  this.canvasCtx.drawImage(this.textSprite,
+    textSourceX, textSourceY, textSourceWidth, textSourceHeight,
+    textTargetX, textTargetY, textTargetWidth, textTargetHeight);
+  this.canvasCtx.drawImage(this.restartImg, 0, 0,
+    restartSourceWidth, restartSourceHeight,
+    restartTargetX, restartTargetY, dimensions.RESTART_WIDTH,
+    dimensions.RESTART_HEIGHT);
+};
