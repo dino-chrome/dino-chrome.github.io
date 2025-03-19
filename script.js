0a760c127cf8068ea82ec477e685c12a226037b3
chromedino <abrarulhaqabrar51@gmail.com>
2025-03-20 00:20:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function adjustGameSize() {
         // Only adjust if not in fullscreen mode
         if (!document.fullscreenElement && !document.webkitFullscreenElement && !document.mozFullScreenElement) {
             const isMobile = window.innerWidth <= 768;
-            const baseHeight = isMobile ? 400 : 600; // Use the heights: 200px for mobile, 350px for desktop
+            const baseHeight = isMobile ? 350 : 610; // Use the heights: 200px for mobile, 350px for desktop
             iframe.style.height = `${baseHeight}px`;
             // Maintain a 16:9 aspect ratio for the iframe (width = height * 16/9)
             const containerWidth = baseHeight * (16 / 9);
